657a99818a2d6238317e88eb39e3b4cf10be387e
Skkii1003 <williampei1003@hotmail.com>
2023-01-05 11:09:40 +0800

    feat:index_split

--- examples/jina_example/data_split.py
+++ examples/jina_example/data_split.py
@@ -1,25 +1,27 @@
 import gzip
 import json
 
+from tqdm import tqdm
+
 
 def save(filename, data):
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         f.write(data)
 
 
 if __name__ == '__main__':
-    len = 0;
+    len = 0
     with gzip.open('corpus-enwiki-20200511-cirrussearch-parasv2.jsonl.gz', "r") as f:
-        for i, line in enumerate(f):
-            len = i + 1;
-        print('The total length is ' + str(i) + '\n')
+        len = 36617357
+        print('The total length is ' + str(len) + '\n')
         split_num = 0
         save_name = "wiki"
         save_num = 0
-        for i, line in enumerate(f):
+        for i, line in enumerate(tqdm(f)):
             para_json = json.loads(line)
-            data = json.dump(para_json)
-            if save_num >= (len / 5):
+            data = json.dumps(para_json)
+            if save_num >= (len / 7):
                 save_num = 0
                 split_num += 1
             save(save_name + str(split_num), data)
+            save_num += 1
--- examples/jina_example/myapp.py
+++ examples/jina_example/myapp.py
@@ -41,6 +41,33 @@ def index(file_name):
     with f:
         f.post(on='/index', inputs=readFile(file_name), show_progress=True, request_size=32)
 
+def index_split(file_name):
+    def readFile(file_name):
+        with open(file_name, "r") as f:
+            for i, line in enumerate(f):
+                try:
+                    para_json = json.loads(line)
+                    doc = Document(
+                        id=f'{i}',
+                        tags={'docId': para_json["docid"],
+                              'secId': para_json["secid"],
+                              'headerId': para_json["headerid"],
+                              'paraId': para_json["para_id"],
+                              'title': para_json["title"],
+                              'section': para_json.get("section") or "",
+                              'subsection': " :: ".join(para_json.get("headers")) or "",
+                              'text': para_json["para"]
+                              }
+                    )
+                    yield doc
+                except:
+                    print(f'skip line {i}')
+                    continue
+
+    f = Flow().load_config('flows/index.yml')
+    with f:
+        f.post(on='/index', inputs=readFile(file_name), show_progress=True, request_size=32)
+
 
 # 搜索并排序
 def query():
@@ -91,6 +118,10 @@ def main(task):
     if task == 'index':
         if Path('./workspace').exists():
             print('./workspace exists, please deleted it if you want to reindexi')
+        data_fn = sys.argv[2] if len(sys.argv) >= 3 else 'toy_data/test.tsv'
+        print(f'indexing {data_fn}')
+        index(data_fn)
+    elif task == 'index_split':
         data_fn = sys.argv[2] if len(sys.argv) >= 3 else 'toy_data/test.tsv'
         print(f'indexing {data_fn}')
         index(data_fn)
