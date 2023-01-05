0fe8b0fc6426236a605d0406ca6dbc12a540b3ae
Skkii1003 <williampei1003@hotmail.com>
2023-01-05 22:06:17 +0800

    syn

--- examples/jina_example/data_split.py
+++ examples/jina_example/data_split.py
@@ -15,12 +15,13 @@ if __name__ == '__main__':
         len = 36617357
         print('The total length is ' + str(len) + '\n')
         split_num = 0
-        save_name = "wiki"
+        save_name = "Wiki_split/wiki"
         save_num = 0
         for i, line in enumerate(tqdm(f)):
             para_json = json.loads(line)
             if save_num >= (len / 7):
                 save_num = 0
                 split_num += 1
-            save(save_name + str(split_num), para_json)
+            save_path = save_name + str(split_num) + '.json'
+            save(save_path, para_json)
             save_num += 1
